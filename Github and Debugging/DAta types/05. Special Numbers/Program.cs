99805dc2d7cd6af65e0408fd35cdbf0881e2cb82
Lubaka <lubosheff@yahoo.com>
2016-05-19 20:10:37 +0300

    DAta

---
+++ Github and Debugging/DAta types/05. Special Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                int sumOfDigits = 0;
+
+
+            }
+            
+        }
+    }
+}
