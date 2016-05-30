c78d70704d675c335185eb9ef6cf8f55f37e00e9
Lubaka <lubosheff@yahoo.com>
2016-05-30 17:40:54 +0300

    changes

---
+++ Arrays/Exercises Arrays/4. Sieve of Eratosthenes/4. Sieve of Eratosthenes.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Sieve_of_Eratosthenes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= n; i++)
+            {
+                if (IsPrimeNumber(i))
+                {
+                    Console.Write(i + " ");
+                }
+            }
+
+        }
+
+        static bool IsPrimeNumber(int num)
+
+        {
+
+            bool bPrime = true;
+
+            int factor = num / 2;
+
+
+
+            int i = 0;
+
+
+
+            for (i = 2; i <= factor; i++)
+
+            {
+
+                if ((num % i) == 0)
+
+                    bPrime = false;
+
+            }
+
+            return bPrime;
+
+        }
+
+    }
+}
---
+++ Arrays/Exercises Arrays/5. Compare Char Arrays/5. Compare Char Arrays.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.Compare_Char_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] arr = Console.ReadLine().Split(' ').Select(char.Parse).ToArray();
+            char[] arr2 = Console.ReadLine().Split(' ').Select(char.Parse).ToArray();
+            Console.WriteLine(String.Join(" ",)
+        }
+        static void CompareChar(char [] arr,char[] arr2)
+        {
+            int min = Math.Min(arr.Length, arr2.Length);
+            List<char> az = new List<char>();
+            for (int i = 0; i < min; i++)
+            {
+                     if (arr[i] > arr2 [i])
+                 {
+                        
+                    }
+                }
+            }
+            
+            
+            
+        }
+    }
+}
