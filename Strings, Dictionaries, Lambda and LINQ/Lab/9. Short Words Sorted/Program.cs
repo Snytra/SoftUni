60235d1ad830981f289917792e642b98fc5ed882
Lubaka <lubosheff@yahoo.com>
2016-06-08 18:00:38 +0300

    update

---
+++ Strings, Dictionaries, Lambda and LINQ/Lab/9. Short Words Sorted/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9.Short_Words_Sorted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] separators = ".,:;()[]\"'\\/!? ".ToCharArray();
+
+            var words = Console.ReadLine()
+                .ToLower()
+                .Split(separators, StringSplitOptions.RemoveEmptyEntries)
+                .Where(x => x.Length < 5)
+                .OrderBy(x => x)
+                .Distinct()
+                .ToList();
+
+            Console.WriteLine(String.Join(", ", words));
+        }
+    }
+}
