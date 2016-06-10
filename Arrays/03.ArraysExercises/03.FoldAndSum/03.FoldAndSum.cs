6b60d1e2b8ae4df23d92f1e3b87259eb736d6610
Lubaka <lubosheff@yahoo.com>
2016-06-10 20:36:27 +0300

    update

---
+++ Arrays/03.ArraysExercises/03.FoldAndSum/03.FoldAndSum.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.FoldAndSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(' ')
+                .Select(int.Parse).ToArray();
+
+            int k = arr.Length / 2;
+            int splitK = k / 2;
+
+            
+            List<int> middle = new List<int>();
+            List<int> outsideFirst = new List<int>();
+            List<int> outsideSecond = new List<int>();
+            List<int> sumOutside = new List<int>();
+
+            for (int i = 0 ; i < arr.Length; i++)
+            {
+                if (i>=k-splitK && i<=k+splitK-1)
+                {
+                    middle.Add(arr[i]);
+                }
+                else if(i >= 0 && i <= splitK)
+                {
+                    outsideFirst.Add(arr[i]);
+                }
+                else
+                {
+                    outsideSecond.Add(arr[i]);
+                }
+            }
+
+            outsideFirst.Reverse();
+            outsideSecond.Reverse();
+
+            foreach (var item in outsideFirst)
+            {
+                sumOutside.Add(item);
+
+            }
+
+            foreach (var item in outsideSecond)
+            {
+                sumOutside.Add(item);
+            }
+
+            var sum = middle.Zip(sumOutside, (x, y) => x + y).ToList();
+
+           
+
+            foreach (var item in sum)
+            {
+                Console.Write(item+" ");
+            }
+        }
+
+    }
+}
---
+++ Arrays/03.ArraysExercises/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MaxSequenceofIncreasingElements
+{
+    class Program
+    {
+        static void Main()
+        {
+          int[] arr = Console.ReadLine()
+                      .Split(' ')
+                      .Select(int.Parse)
+                      .ToArray();
+          int[] arr2 =
+        }
+    }
+}
